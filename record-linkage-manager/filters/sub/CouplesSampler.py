cbb71090ec1345c11b11c3e16f430135a12c9cb3
Unofficial Webpipe <fippidaniele@hotmail.it>
2023-11-16 11:46:14 +0100

    Sampling is working

--- record-linkage-manager/filters/sub/CouplesSampler.py
+++ record-linkage-manager/filters/sub/CouplesSampler.py
@@ -1,23 +1,118 @@
+from itertools import combinations
+
 import pandas as pd
+from nltk import ngrams
 
 
 class CouplesSampler:
 
     def __init__(self):
-        self.positives = 0.6
         self.max_samples = 1000
+        self.top_rows = int(0.5 * self.max_samples)
+        self.bottom_rows = int(0.3 * self.max_samples)
+        self.middle_rows = int((1 - self.top_rows - self.bottom_rows) * self.max_samples)
 
     def sample_couples(self, dfs, important_attributes):
+        big_schema = self.__compute_big_schema(dfs)
+        terms_weights = self.__compute_terms_weights(big_schema, important_attributes)
 
-        big_schema = pd.DataFrame()
-        
+        merged_df = pd.merge(big_schema, big_schema, how='cross', suffixes=('_l', '_r'))
+        merged_df = merged_df[(merged_df['table_id_l'] != merged_df['table_id_r'])]
 
-        pass
+        important_attributes_dupl = self.__transform_list(important_attributes)
+        merged_df = merged_df.dropna(subset=important_attributes_dupl)
 
+        merged_df['distance'] = merged_df.apply(self.__compute_jaccard_distance,
+                                                args=(important_attributes, terms_weights), axis=1)
+        merged_df = merged_df.sort_values(by='distance')
 
-    def __sample_positive_couples(self, dfs, important_attributes):
-        selected_columns = important_attributes
+        top_df = merged_df.head(self.top_rows)
+        middle_df = merged_df.iloc[int((len(merged_df)/2 - self.middle_rows / 2)):int((len(merged_df)/2 + self.middle_rows / 2))]
+        bottom_df = merged_df.iloc[int(len(merged_df)/2):].sample(n=self.bottom_rows)
 
+        result_df = pd.concat([top_df, middle_df, bottom_df])
+        result_df = result_df.reset_index(drop=True)
 
+        result_df = result_df.drop(columns=['distance'])  # do i really need to drop it?
 
-        pass
+        return result_df
+
+    def __compute_big_schema(self, dfs):
+        mediated_schema_column_names = dfs[0].columns.tolist()
+        mediated_schema_column_names.append("table_id")
+
+        big_schema = pd.DataFrame(columns=mediated_schema_column_names)
+
+        i = 0
+        for df in dfs:
+            df["table_id"] = i
+            df = df.loc[:, ~df.columns.duplicated()]
+            df.reset_index(drop=True, inplace=True)
+            big_schema = pd.concat([big_schema, df], join='outer', ignore_index=True)
+            big_schema.reset_index(inplace=True, drop=True)
+            i += 1
+
+        return big_schema
+
+    def __compute_jaccard_distance(self, row, important_attributes, terms_weights):
+        jaccard_sum = 0
+        divisor = len(important_attributes)
+
+        for attribute in important_attributes:
+            str1 = row[attribute + "_l"]
+            str2 = row[attribute + "_r"]
+            jaccard_sum += self.__jaccard_distance(str1, str2, terms_weights[attribute])
+
+        return jaccard_sum / divisor
+
+    def __jaccard_distance(self, str1, str2, terms_weight):
+        tokens1 = set(str(str1).lower().split())
+        tokens2 = set(str(str2).lower().split())
+
+        intersection_weight = 0
+        union_weight = 0
+
+        for token in tokens1.intersection(tokens2):
+            intersection_weight += terms_weight[token]
+
+        for token in tokens1.union(tokens2):
+            union_weight += terms_weight[token]
+
+        jaccard_distance = 1 - (intersection_weight / union_weight)
+        return jaccard_distance
+
+    def __transform_list(self, input_list):
+        transformed_list = [f'{item}_l' for item in input_list] + [f'{item}_r' for item in input_list]
+        return transformed_list
+
+    def __compute_terms_df(self, big_schema, important_attributes):
+        term_count_df = {}
+
+        for attribute in important_attributes:
+            term_count_df[attribute] = {}
+            for cell in big_schema[attribute]:
+                tokens = set(str(cell).split())
+                for token in tokens:
+                    if token in term_count_df[attribute]:
+                        term_count_df[attribute][token] += 1
+                    else:
+                        term_count_df[attribute][token] = 1
+
+        return term_count_df
+
+    def __compute_terms_weights(self, big_schema, important_attributes):
+        term_count_df = self.__compute_terms_df(big_schema, important_attributes)
+        num_of_documents = len(big_schema)
+
+        term_weights = {}
+
+        for k, v in term_count_df.items():
+            attribute = k
+            terms_df = v
+
+            term_weights[attribute] = {}
+
+            for term, df in terms_df.items():
+                term_weights[attribute][term.lower()] = 1 - (df / num_of_documents)
+
+        return term_weights
---
+++ record-linkage-manager/tests/test_filters.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+
+import pandas as pd
+
+from filters.sub.CouplesSampler import CouplesSampler
+
+
+class FiltersTest(unittest.TestCase):
+
+    def test_jobs(self):
+        important_attributes = ["Company Names"]
+        self.run_job("jobs", important_attributes)
+
+    def test_companies(self):
+        important_attributes = ["Name"]
+        self.run_job("companies", important_attributes)
+
+    def test_companies2(self):
+        important_attributes = ["Name"]
+        self.run_job("companies2", important_attributes)
+
+    def run_job(self, group_name, important_attributes):
+        folder_path_in = f"datasets/input/{group_name}"
+        folder_path_out = f"datasets/output/{group_name}"
+
+        files = os.listdir(folder_path_in)
+        csv_files = [file for file in files if file.endswith('.csv')]
+
+        dataframes = []
+
+        for file in csv_files:
+            file_path = os.path.join(folder_path_in, file)
+            df = pd.read_csv(file_path, encoding="ISO-8859-1")
+            dataframes.append(df)
+
+        couples_sample = CouplesSampler()
+        sampled_couples_df = couples_sample.sample_couples(dataframes, important_attributes)
+
+        # Save to output
+        sampled_couples_df.to_csv(folder_path_out + ".csv", index=False)
+
+
